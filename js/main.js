37bdffca143a0493ffed775b97fd9bd340c3964b
Eugene-prog <ieugene.prog@gmail.com>
2021-11-20 23:30:03 +0700

    layout card item, hover-text, basket icon, favorite icon

--- js/main.js
+++ js/main.js
@@ -7,12 +7,16 @@ $(function () {
 			'<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
 	});
 
-	$(".search__tabs-item").on("click", function (e) {
+	$(".tab").on("click", function (e) {
 		e.preventDefault();
-		$(".search__tabs-item").removeClass("search__tabs-item--active");
-		$(".search__content-item").removeClass("search__content-item--active");
+		$(".tab").removeClass("tab--active");
+		$(".tabs-content").removeClass("tabs-content--active");
 
-		$(this).addClass("search__tabs-item--active");
-		$($(this).attr("href")).addClass("search__content-item--active");
+		$(this).addClass("tab--active");
+		$($(this).attr("href")).addClass("tabs-content--active");
+	});
+
+	$(".product-item__favorite").on("click", function () {
+		$(".product-item__favorite").toggleClass("product-item__favorite--active");
 	});
 });
